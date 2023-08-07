85dee2157e784bfc1d5882aa02f066935d817334
Rahul Ragi <rahulragi30@gmail.com>
2023-08-07 12:21:48 -0400

    Testing Commit to GitHub

---
+++ build.sbt
@@ -0,0 +1,14 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.12.18"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "sbt_demo"
+  )
+val sparkVersion = "3.3.2"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion
+)
---
+++ src/main/scala/sparkpack/sparkobj.scala
@@ -0,0 +1,57 @@
+package sparkpack
+
+import org.apache.spark._
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.expressions._
+
+object sparkobj {
+
+  def main(args:Array[String]):Unit={
+
+
+    println("=============Started1============")
+    println
+
+    val conf = new SparkConf().setAppName("ES").setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+
+    val spark = SparkSession.builder().getOrCreate()
+    import spark.implicits._
+
+    println
+
+    val jsondf = spark
+      .read
+      .format("json")
+      .option("multiline", "true")
+      .load("C:/zeyo_data/cm.json")
+
+    jsondf.show()
+    jsondf.printSchema()
+
+
+    val flatdf = jsondf.select(
+      "Technology",
+      "TrainerName",
+      "address.*",
+      "id",
+      "workloc"
+
+    )
+
+    flatdf.show()
+    flatdf.printSchema()
+
+
+
+  }
+
+}
